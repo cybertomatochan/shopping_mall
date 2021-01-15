ee28853f7842d9f515561c417e642a8ad0ada369
YiHsiu-Lu <moretomato123@gmail.com>
2021-01-15 17:58:22 +0800

    0115 change index path

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/shop_test/dist/',
+    assetsPublicPath: '/shopping_mall/',
 
     /**
      * Source Maps
